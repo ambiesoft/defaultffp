36ad21ffe12cbf0aa0836f59bd18a7ccd4d67e6a
fjUnc <fjUnc@THEXP>
2017-12-11 00:16:23 +0900

    second commit

---
+++ defaultffp/FormMain.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace defaultffp
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+            this.btnOK = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.listMain = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtCurDef = new System.Windows.Forms.TextBox();
+            this.btnSRR = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnOK
+            // 
+            resources.ApplyResources(this.btnOK, "btnOK");
+            this.btnOK.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.btnOK.Name = "btnOK";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // btnCancel
+            // 
+            resources.ApplyResources(this.btnCancel, "btnCancel");
+            this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // listMain
+            // 
+            resources.ApplyResources(this.listMain, "listMain");
+            this.listMain.FormattingEnabled = true;
+            this.listMain.Name = "listMain";
+            this.listMain.DoubleClick += new System.EventHandler(this.listMain_DoubleClick);
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // txtCurDef
+            // 
+            resources.ApplyResources(this.txtCurDef, "txtCurDef");
+            this.txtCurDef.Name = "txtCurDef";
+            this.txtCurDef.ReadOnly = true;
+            // 
+            // btnSRR
+            // 
+            resources.ApplyResources(this.btnSRR, "btnSRR");
+            this.btnSRR.Name = "btnSRR";
+            this.btnSRR.UseVisualStyleBackColor = true;
+            this.btnSRR.Click += new System.EventHandler(this.btnSRR_Click);
+            // 
+            // FormMain
+            // 
+            this.AcceptButton = this.btnOK;
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnCancel;
+            this.Controls.Add(this.btnSRR);
+            this.Controls.Add(this.txtCurDef);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listMain);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnOK);
+            this.Name = "FormMain";
+            this.Load += new System.EventHandler(this.FormMain_Load);
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.FormMain_FormClosed);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.ListBox listMain;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtCurDef;
+        private System.Windows.Forms.Button btnSRR;
+    }
+}
+
---
+++ defaultffp/FormMain.cs
@@ -0,0 +1,356 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+using Ambiesoft;
+using System.Diagnostics;
+using Microsoft.Win32;
+namespace defaultffp
+{
+    public partial class FormMain : Form
+    {
+        string ffpath_;
+        string iniFile_;
+        string IniFile
+        {
+            get
+            {
+                if(iniFile_==null)
+                    iniFile_=Path.ChangeExtension(Application.ExecutablePath, "ini");
+                return iniFile_;
+            }
+        }
+        public FormMain()
+        {
+            InitializeComponent();
+
+            // FileInfo fi = new FileInfo(Application.ExecutablePath);
+            // string inipath = Path.ChangeExtension(Application.ExecutablePath, ".ini");
+            // string inipath = Path.Combine(fi.DirectoryName, fi.Name.Replace(fi.Extension, "ini"));
+            Profile.GetString("option", "firefox", "", out ffpath_, IniFile);
+        }
+
+        int originalDefault_ = -1;
+        int currentDefault_ = -1;
+        HashIni ffini_;
+        private string FFPPath
+        {
+            get
+            {
+                string inipath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+                inipath += @"\Mozilla\Firefox\profiles.ini";
+                return inipath;
+            }
+        }
+
+        private void updateList()
+        {
+            if (!File.Exists(FFPPath))
+            {
+                MessageBox.Show(FFPPath + " not found", Application.ProductName,
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Close();
+                return;
+            }
+
+            listMain.Items.Clear();
+            ffini_ = Profile.ReadAll(FFPPath);
+            for (int i = 0; ; ++i)
+            {
+                String app = "Profile" + i.ToString();
+                String name;
+                Profile.GetString(app, "Name", "", out name, ffini_);
+                if (string.IsNullOrEmpty(name))
+                    break;
+
+                listMain.Items.Add(name);
+
+                int ndef;
+                Profile.GetInt(app, "Default", 0, out ndef, ffini_);
+                if (ndef != 0)
+                {
+                    txtCurDef.Text = name;
+                    if (originalDefault_ < 0)
+                        originalDefault_ = i;
+                }
+            }
+        }
+        
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            updateList();
+            int index;
+            Profile.GetInt("option", "index", -1, out index, IniFile);
+            if (0 <= index && index < listMain.Items.Count)
+            {
+                listMain.SelectedIndex = index;
+            }
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private bool doWrite(int nDefault)
+        {
+            if (nDefault < 0)
+            {
+                if (listMain.SelectedIndex < 0)
+                    return false;
+
+                nDefault = listMain.SelectedIndex;
+            }
+
+            for(int i=0 ; i < listMain.Items.Count ; ++i)
+            {
+                String app = "Profile" + i.ToString();
+                Profile.WriteInt(app, "Default", (i == nDefault ? 1 : 0), ffini_);
+            }
+
+            if (!Profile.WriteAll(ffini_, FFPPath))
+                return false;
+
+            currentDefault_ = nDefault;
+            return true;
+
+        }
+        private bool doWrite()
+        {
+            return doWrite(-1);
+        }
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            if (listMain.SelectedIndex >= 0)
+            {
+                if (!doWrite())
+                {
+                    MessageBox.Show("Write failed", Application.ProductName,
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+            }
+            Close();
+        }
+
+        private void listMain_DoubleClick(object sender, EventArgs e)
+        {
+            if (!doWrite())
+            {
+                MessageBox.Show("Write failed", Application.ProductName,
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            updateList();
+        }
+
+        private void btnSRR_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // this.UseWaitCursor = true;
+                Cursor.Current = Cursors.WaitCursor;
+
+                if (listMain.SelectedIndex < 0)
+                    return;
+
+
+                int current = currentDefault_ >= 0 ? currentDefault_ : originalDefault_;
+
+                string filefoxpath = findFirefox();
+                if (string.IsNullOrEmpty(filefoxpath))
+                {
+                    MessageBox.Show(Properties.Resources.FIREFOX_NOT_FOUND,
+                        Application.ProductName,
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Error);
+                    return;
+                }
+
+                Process[] allproc = Process.GetProcessesByName(Path.GetFileNameWithoutExtension(filefoxpath));
+                List<Process> alltokill = new List<Process>();
+                foreach (Process proc in allproc)
+                {
+                    if (0 == string.Compare(proc.MainModule.FileName, filefoxpath, true))
+                    {
+                        alltokill.Add(proc);
+                    }
+                }
+
+                if (alltokill.Count != 0)
+                {
+                    DialogResult result = MessageBox.Show(Properties.Resources.FIREFOX_IS_ALREADY_RUNNING,
+                        Application.ProductName,
+                        MessageBoxButtons.YesNoCancel,
+                        MessageBoxIcon.Question,
+                        MessageBoxDefaultButton.Button3);
+                    if(result==DialogResult.Cancel)
+                    {
+                        return;
+                    }
+                    if (result == DialogResult.Yes)
+                    {
+                        foreach (Process p in alltokill)
+                        {
+                            try
+                            {
+                                p.Kill();
+                            }
+                            catch (Exception ex)
+                            { 
+                            }
+                        }
+                    }
+                }
+
+                if (!doWrite())
+                {
+                    MessageBox.Show("Write failed", Application.ProductName,
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+
+                ProcessStartInfo psi = new ProcessStartInfo();
+                psi.FileName = filefoxpath;
+                psi.UseShellExecute = true;
+                Process firefox;
+                try
+                {
+                    firefox = Process.Start(psi);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, Application.ProductName,
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                if (firefox == null)
+                {
+                    System.Threading.Thread.Sleep(5000);
+                }
+                else
+                {
+                    firefox.WaitForInputIdle();
+                }
+
+
+                if (!doWrite(current))
+                {
+                    MessageBox.Show("Write failed", Application.ProductName,
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+            }
+            finally
+            {
+                // this.UseWaitCursor = false;
+                Cursor.Current = Cursors.Default;
+            }
+            Close();
+        }
+
+        private string findFirefoxFromReg()
+        {
+            RegistryKey regkey = Registry.CurrentUser.OpenSubKey(@"Software\Mozilla\Mozilla Firefox", false);
+            if (regkey == null)
+                return null;
+
+            foreach (string sub in regkey.GetSubKeyNames())
+            {
+                RegistryKey k = regkey.OpenSubKey(sub);
+                if (k != null)
+                {
+                    RegistryKey main = k.OpenSubKey("Main");
+                    if (main != null)
+                    {
+                        object o = main.GetValue("PathToExe");
+                        if (o != null)
+                        {
+                            return o.ToString();
+                        }
+                    }
+                }
+            }
+            return null;
+        }
+        private string findFirefox()
+        {
+            if (File.Exists(ffpath_))
+                return ffpath_;
+
+            string tryffpath = findFirefoxFromReg();
+
+            //OpenFileDialogクラスのインスタンスを作成
+            using (OpenFileDialog ofd = new OpenFileDialog())
+            {
+                
+                //はじめのファイル名を指定する
+                //はじめに「ファイル名」で表示される文字列を指定する
+                ofd.FileName = Path.GetFileName(tryffpath) ?? "firefox.exe";
+                
+                //はじめに表示されるフォルダを指定する
+                //指定しない（空の文字列）の時は、現在のディレクトリが表示される
+                ofd.InitialDirectory = Path.GetDirectoryName(tryffpath);
+                
+                //[ファイルの種類]に表示される選択肢を指定する
+                //指定しないとすべてのファイルが表示される
+                ofd.Filter = "Firefox Executable (*.exe)|*.exe";
+                
+                //[ファイルの種類]ではじめに選択されるものを指定する
+                //2番目の「すべてのファイル」が選択されているようにする
+                //ofd.FilterIndex = 2;
+                //タイトルを設定する
+                ofd.Title = "Choose firefox.exe";
+                
+                //ダイアログボックスを閉じる前に現在のディレクトリを復元するようにする
+                ofd.RestoreDirectory = true;
+                
+                //存在しないファイルの名前が指定されたとき警告を表示する
+                //デフォルトでTrueなので指定する必要はない
+                ofd.CheckFileExists = true;
+                
+                //存在しないパスが指定されたとき警告を表示する
+                //デフォルトでTrueなので指定する必要はない
+                ofd.CheckPathExists = true;
+
+                //ダイアログを表示する
+                if (ofd.ShowDialog() != DialogResult.OK)
+                {
+                    return string.Empty;
+                }
+                ffpath_ = ofd.FileName;
+            }
+
+            
+            if (!Profile.WriteString("option", "firefox", ffpath_, IniFile))
+            {
+                MessageBox.Show("save failed",
+                    Application.ProductName,
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+            }
+            // return @"C:\LegacyPrograms\Mozilla Firefox 32\firefox.exe";
+            // return "firefox.lnk";
+            return ffpath_;
+        }
+
+        private void FormMain_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (!Profile.WriteInt("option", "index", listMain.SelectedIndex, IniFile))
+            {
+                MessageBox.Show("save failed",
+                  Application.ProductName,
+                  MessageBoxButtons.OK,
+                  MessageBoxIcon.Error);
+            }
+        }
+
+
+    }
+}
